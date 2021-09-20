e3ffaef08917a6c254eccfff279caa4baa26b778
manimovassagh <mani.mgh@gmail.com>
2021-09-20 16:57:37 +0200

    33

---
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/MultipleSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.manimovassagh.multipleSpringAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/manimovassagh/multipleSpringAPI/MultipleSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.manimovassagh.multipleSpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
