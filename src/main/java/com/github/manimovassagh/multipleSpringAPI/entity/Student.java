2ab8ba10be5c65ac5b6a56576d1019c5620bf6d8
manimovassagh <mani.mgh@gmail.com>
2021-09-20 17:23:38 +0200

    77

--- src/main/java/com/github/manimovassagh/multipleSpringAPI/entity/Student.java
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/entity/Student.java
@@ -13,14 +13,24 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-//@Table(name = "tbl_student") If you want to change the table name or column this is a way
+@Table(name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(name = "emailid_unique",
+                columnNames = "email_address"))
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence")
     private Long studentId;
     private String firstName;
     private String lastname;
+    @Column(name = "email_address",nullable = false)
+    private String emailId;
     private String guardianName;
     private String guardianEmail;
     private String guardianMobile;
