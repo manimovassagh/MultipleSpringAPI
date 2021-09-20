117134876834a81cdc661fdc99ff74754f4f2e5e
manimovassagh <mani.mgh@gmail.com>
2021-09-20 17:10:41 +0200

    66

--- src/main/java/com/github/manimovassagh/multipleSpringAPI/entity/Student.java
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/entity/Student.java
@@ -6,16 +6,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+//@Table(name = "tbl_student") If you want to change the table name or column this is a way
 public class Student {
 
     @Id
