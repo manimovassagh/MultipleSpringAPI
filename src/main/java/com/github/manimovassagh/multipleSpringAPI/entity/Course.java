22f1f4766fbb236be354c565e4d671198bc7131d
manimovassagh <mani.mgh@gmail.com>
2021-09-21 13:50:30 +0200

    one to one

---
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/entity/Course.java
@@ -0,0 +1,26 @@
+package com.github.manimovassagh.multipleSpringAPI.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+
+
+    @Id
+    @SequenceGenerator(name = "course_sequence", sequenceName = "course_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_sequence")
+    private Long courseId;
+    private String courseTitle;
+    private Integer credit;
+
+}
