5c0094fd35d44c740938c7c88a107d5c0871cdf9
manimovassagh <mani.mgh@gmail.com>
2021-09-21 12:01:07 +0200

    7777

--- src/main/java/com/github/manimovassagh/multipleSpringAPI/repository/StudentRepository.java
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/repository/StudentRepository.java
@@ -1,16 +1,18 @@
 package com.github.manimovassagh.multipleSpringAPI.repository;
 
 import com.github.manimovassagh.multipleSpringAPI.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
-    public List<Student> findByFirstName(String firstName);
-    public List<Student> findByFirstNameContaining(String name);
-    public List<Student> findByLastNameNotNull();
-    public List<Student> findByGuardianName(String guardianName);
+    List<Student> findByFirstName(String firstName);
+    List<Student> findByFirstNameContaining(String name);
+    List<Student> findByLastNameNotNull();
+    List<Student> findByGuardianName(String guardianName);
 
-}
+    }
