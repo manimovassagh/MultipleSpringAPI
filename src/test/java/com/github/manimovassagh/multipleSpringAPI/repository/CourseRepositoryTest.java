c84ea0efe2f894d4d50189326509476444525a9b
manimovassagh <mani.mgh@gmail.com>
2021-09-22 16:08:41 +0200

    test added

--- src/test/java/com/github/manimovassagh/multipleSpringAPI/repository/CourseRepositoryTest.java
+++ src/test/java/com/github/manimovassagh/multipleSpringAPI/repository/CourseRepositoryTest.java
@@ -1,4 +1,10 @@
 package com.github.manimovassagh.multipleSpringAPI.repository;
 
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CourseRepositoryTest {
+
+
 }
