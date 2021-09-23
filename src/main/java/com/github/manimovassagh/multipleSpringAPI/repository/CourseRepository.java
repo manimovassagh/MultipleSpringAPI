af3e383472aa6d559501609c85a0339b100a2ea1
manimovassagh <mani.mgh@gmail.com>
2021-09-23 15:58:07 +0200

    jj

--- src/main/java/com/github/manimovassagh/multipleSpringAPI/repository/CourseRepository.java
+++ src/main/java/com/github/manimovassagh/multipleSpringAPI/repository/CourseRepository.java
@@ -1,15 +1,13 @@
 package com.github.manimovassagh.multipleSpringAPI.repository;
 
 import com.github.manimovassagh.multipleSpringAPI.entity.Course;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course,Long> {
+public interface CourseRepository extends JpaRepository<Course,Long>, JpaSpecificationExecutor<Course> {
 
 
 }
